c61fe2613f8fcc6450cd3369cc6a4eb2a62f2fb9
fmohamed <fmohamed@entrepreware.com>
2020-05-27 13:06:11 +0200

    Create Cafeteria-products page

---
+++ src/app/pages/cafeteria-products/cafeteria-products.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cafeteria-products',
+  templateUrl: './cafeteria-products.page.html',
+  styleUrls: ['./cafeteria-products.page.scss'],
+})
+export class CafeteriaProductsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
