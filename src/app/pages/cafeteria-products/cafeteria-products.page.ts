c7e54108d1f7810b5d5e4478fc9424ba1362b964
fmohamed <fmohamed@entrepreware.com>
2020-05-27 18:09:18 +0200

    WIP: Retrieve categories & products

--- src/app/pages/cafeteria-products/cafeteria-products.page.ts
+++ src/app/pages/cafeteria-products/cafeteria-products.page.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { CafeteriaService } from 'src/app/services/Cafeteria/cafeteria.service';
+import { LoadingViewService } from 'src/app/services/LoadingView/loading-view.service';
 
 @Component({
-  selector: 'app-cafeteria-products',
-  templateUrl: './cafeteria-products.page.html',
-  styleUrls: ['./cafeteria-products.page.scss'],
+  selector: "app-cafeteria-products",
+  templateUrl: "./cafeteria-products.page.html",
+  styleUrls: ["./cafeteria-products.page.scss"],
 })
 export class CafeteriaProductsPage implements OnInit {
-
-  constructor() { }
+  categories;
+  selectedCategory;
+  products;
+  constructor(
+    private cafeteriaService: CafeteriaService,
+    private load: LoadingViewService
+  ) {}
 
   ngOnInit() {
+    this.getCategories();
   }
 
-  getCategories(){}
+  async getCategories() {
+    this.load.startNormalLoading("Loading Products...");
+    this.categories = await this.cafeteriaService
+      .getCafeteriaProducts();
+    this.selectedCategory = this.categories[0];
+    this.products = await this.cafeteriaService
+      .getCafeteriaCategoryProducts(this.selectedCategory.id)
+      .toPromise();
+    this.load.stopLoading();
+  }
 
+  segmentChanged(event){
+    console.log("segmentChanged:", event);
+  }
 }
--- src/app/services/Cafeteria/cafeteria.service.ts
+++ src/app/services/Cafeteria/cafeteria.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AccountService } from '../Account/account.service';
+import { HttpClient } from '@angular/common/http';
+import { Url_domain } from 'src/app/models/url_domain';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CafeteriaService {
+  DomainUrl: Url_domain;
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private http: HttpClient
+  ) {
+    this.DomainUrl = new Url_domain();
+  }
+
+  getCafeteriaCategories() {
+    return this.http.get(
+      this.DomainUrl.Domain +
+        "/authentication/CafeteriaPos.ent/getCafeteriaCategories.ent?branchId=" +
+        this.accountService.userBranchId
+    );
+  }
+
+  getCafeteriaCategoryProducts(categoryId) {
+    return this.http.get(
+      this.DomainUrl.Domain +
+        "/authentication/CafeteriaPos.ent/getCafeteriaCategoryProducts.ent?categoryId=" +
+        categoryId
+    );
+  }
+
+  async getCafeteriaProducts() {
+    const categories = await this.getCafeteriaCategories().toPromise() as any[];
+    const promisesArray = [];
+    for(let category of categories){
+      promisesArray.push(this.getCafeteriaCategoryProducts(category.id).toPromise());
+    }
+    const result = await Promise.all(promisesArray);
+    return result;
+  }
 }
