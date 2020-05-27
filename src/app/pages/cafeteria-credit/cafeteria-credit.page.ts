502a4192b08fbf68174e78eca3ce2f48b4e1a5ca
fmohamed <fmohamed@entrepreware.com>
2020-05-27 13:54:22 +0200

    Create CafeteriaCredit page

---
+++ src/app/pages/cafeteria-credit/cafeteria-credit.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cafeteria-credit',
+  templateUrl: './cafeteria-credit.page.html',
+  styleUrls: ['./cafeteria-credit.page.scss'],
+})
+export class CafeteriaCreditPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
