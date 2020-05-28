741eb3cf0999c070f3927affd6ce5fd4e90504ca
fmohamed <fmohamed@entrepreware.com>
2020-05-28 14:54:18 +0200

    Get all categories & products at once Display category products on segment change

--- src/app/pages/cafeteria-products/cafeteria-products.page.ts
+++ src/app/pages/cafeteria-products/cafeteria-products.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CafeteriaService } from 'src/app/services/Cafeteria/cafeteria.service';
 import { LoadingViewService } from 'src/app/services/LoadingView/loading-view.service';
+import { CafeteriaCategory } from 'src/app/models/cafeteria_category';
 
 @Component({
   selector: "app-cafeteria-products",
@@ -8,9 +9,8 @@ import { LoadingViewService } from 'src/app/services/LoadingView/loading-view.se
   styleUrls: ["./cafeteria-products.page.scss"],
 })
 export class CafeteriaProductsPage implements OnInit {
-  categories;
-  selectedCategory;
-  products;
+  categories: CafeteriaCategory[];
+  selectedCategory: CafeteriaCategory;
   constructor(
     private cafeteriaService: CafeteriaService,
     private load: LoadingViewService
@@ -22,16 +22,13 @@ export class CafeteriaProductsPage implements OnInit {
 
   async getCategories() {
     this.load.startNormalLoading("Loading Products...");
-    this.categories = await this.cafeteriaService
-      .getCafeteriaProducts();
+    this.categories = await this.cafeteriaService.getCafeteriaProducts();
     this.selectedCategory = this.categories[0];
-    this.products = await this.cafeteriaService
-      .getCafeteriaCategoryProducts(this.selectedCategory.id)
-      .toPromise();
     this.load.stopLoading();
   }
 
-  segmentChanged(event){
+  segmentChanged(index){
     console.log("segmentChanged:", event);
+    this.selectedCategory = this.categories[index];
   }
 }
