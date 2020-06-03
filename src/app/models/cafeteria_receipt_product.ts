e38aaadb67f82619467a97e43d1d9003cf1d6473
fmohamed <fmohamed@entrepreware.com>
2020-06-03 12:31:07 +0200

    Create CafeteriaReceiptProduct model

---
+++ src/app/models/cafeteria_receipt_product.ts
@@ -0,0 +1,11 @@
+import { CafeteriaProduct } from './cafeteria_product';
+
+export class CafeteriaReceiptProduct {
+  id: any;
+  cost: number;
+  price: number;
+  quantity: number;
+  product: CafeteriaProduct;
+
+  constructor() {}
+}
