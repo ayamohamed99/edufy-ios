4b74facb680b818144d66a4c6836db0be8d16820
fmohamed <fmohamed@entrepreware.com>
2020-06-03 12:30:53 +0200

    Create CafeteriaCard model

---
+++ src/app/models/cafeteria_card.ts
@@ -0,0 +1,11 @@
+export class CafeteriaCard {
+  id: number;
+  branchId: number;
+  barCode: string;
+  credit: number;
+  discount: number;
+  receiptSet: any;
+  cafeteriaCardRechargeHistories: [];
+
+  constructor() {}
+}
