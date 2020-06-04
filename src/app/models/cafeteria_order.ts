d8d99c85ec1bfb9e1dbcd6b54f2d5886ca613042
fmohamed <fmohamed@entrepreware.com>
2020-06-04 16:02:01 +0200

    WIP Modify Cafeteris Order model

--- src/app/models/cafeteria_order.ts
+++ src/app/models/cafeteria_order.ts
@@ -1,9 +1,18 @@
 
 import {CafeteriaProduct} from './cafeteria_product';
+import { CafeteriaCard } from './cafeteria_card';
 
 export class CafeteriaOrder {
   id: any;
+  card: CafeteriaCard;
+  creationDate: number;
+  discount: number;
   branchId: number;
+  status: string;
+  total: number;
+  subTotal: number;
+  tax: number;
+  user: any;
   products: CafeteriaProduct[];
   comment: string;
 
