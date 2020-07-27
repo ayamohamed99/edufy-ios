c07cab25a7b04b18e0daa48962430c547b83b840
lyoussef <marabi@entrepreware.com>
2020-07-27 09:46:32 +0200

    Cafeteria work - Version 0.0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
 // export const DOMAIN_URL = 'http://104.155.139.135:8080';
 
 // production from mobile
-// export const DOMAIN_URL = "http://104.198.175.198";
+export const DOMAIN_URL = "http://104.198.175.198";
 
 // local from mobile
-export const DOMAIN_URL = 'http://192.168.1.28:9090';
+// export const DOMAIN_URL = 'http://192.168.1.28:9090';
