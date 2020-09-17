837b839278f7114150c28e60bd545aa34714232a
lyoussef <marabi@entrepreware.com>
2020-09-17 12:37:34 +0200

    add ionic badge to menu

--- src/app/services/refresh/refresh.service.ts
+++ src/app/services/refresh/refresh.service.ts
@@ -8,10 +8,12 @@ export class RefreshService {
   
   public noOfUnSeenMessages ;
   constructor(private chatService: ChatService) { }
-
+  public hasUnSeenMessages = false;
   refreshNoOfUnseenMessages(){
     this.chatService.getNumberOfUnseenMessages().subscribe(val => {
       this.noOfUnSeenMessages = val;
+      if(this.noOfUnSeenMessages == 0) this.hasUnSeenMessages = false;
+      else this.hasUnSeenMessages = true;
     })
   };
 }
